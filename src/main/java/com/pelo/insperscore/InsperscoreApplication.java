5421ad449eb0a3dd3736e747518e9efa2366b2f6
Nicolas Volf <nicolas.vfs15@gmail.com>
2025-10-28 17:32:54 -0300

    Primeiro commit

---
+++ src/main/java/com/pelo/insperscore/InsperscoreApplication.java
@@ -0,0 +1,13 @@
+package com.pelo.insperscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsperscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsperscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pelo/insperscore/InsperscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelo.insperscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsperscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
